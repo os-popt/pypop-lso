4679276a485b242ab6bf6db2432d528ea9bf6898
os-popt <11749325@mail.sustech.edu.cn>
2020-03-19 00:44:06 +0800

    fix a serious bug

--- benchmarks/continuous_functions.py
+++ benchmarks/continuous_functions.py
@@ -4,11 +4,12 @@ import numpy as np
 # helper function
 def _transform_and_check(x):
     """transform the input into the numpy.ndarray type, if necessary, and then
-        check whether or not the number of dimensions is 1. If not, raise a TypeError.
+        check whether or not the number of dimensions <= 1. If not, raise a TypeError.
     """
     x = np.squeeze(x)
-    if x.ndim != 1:
-        raise TypeError("the number of dimensions should equal 1.")
+    if x.ndim > 1:
+        raise TypeError("the number of dimensions should <= 1 " +
+            "after squeezing the input via numpy.squeeze().")
     return x
 
 
