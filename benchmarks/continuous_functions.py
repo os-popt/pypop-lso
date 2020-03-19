5eed6841bdee579233daac70242192e9b6717299
os-popt <11749325@mail.sustech.edu.cn>
2020-03-19 20:09:45 +0800

    add the function ellipsoid

--- benchmarks/continuous_functions.py
+++ benchmarks/continuous_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-# helper function
+# helper functions
 def _transform_and_check(x):
     """transform the input into the numpy.ndarray type, if necessary, and then
         check whether or not the number of dimensions <= 1. If not, raise a TypeError.
@@ -12,8 +12,23 @@ def _transform_and_check(x):
             "after squeezing the input via numpy.squeeze().")
     return x
 
+def _transform_and_check_size(x):
+    """Check whether or not the size >= 2. If not, raise a TypeError.
+    """
+    x = _transform_and_check(x)
+    if x.size < 2:
+        raise TypeError("the size should >= 2 after squeezing the input " +
+            "via numpy.squeeze().")
+    return x
+
 
 def sphere(x):
     x = _transform_and_check(x)
     y = np.sum(np.power(x, 2))
+    return y
+
+def ellipsoid(x):
+    x = _transform_and_check_size(x)
+    weights = np.power(10, 6 * np.linspace(0, 1, num=x.size))
+    y = np.dot(weights, np.power(x, 2))
     return y
