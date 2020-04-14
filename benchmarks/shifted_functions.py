a89792a27c7ca62d4276755754aae2027ff37c4a
os-popt <11749325@mail.sustech.edu.cn>
2020-04-14 21:18:13 +0800

    improve the input func

--- benchmarks/shifted_functions.py
+++ benchmarks/shifted_functions.py
@@ -25,7 +25,7 @@ def _load_shift_vector(func, x, shift_vector=None):
     return x
 
 
-def generate_shift_vector(func_name, n_dim, low, high):
+def generate_shift_vector(func, n_dim, low, high):
     low, high = _transform_and_check(low), _transform_and_check(high)
     if low.size == 1:
         low = np.tile(low, n_dim)
@@ -35,11 +35,13 @@ def generate_shift_vector(func_name, n_dim, low, high):
         raise TypeError("low'size should equal n_dim.")
     if high.size != n_dim:
         raise TypeError("high'size should equal n_dim.")
+    if hasattr(func, '__call__'):
+        func = func.__name__
     data_folder = "po_input_data"
     if not os.path.exists(data_folder):
         os.mkdir(data_folder)
     data_path = os.path.join(data_folder,
-        "shift_vector___" + func_name + "_dim_" + str(n_dim) + ".txt")
+        "shift_vector___" + func + "_dim_" + str(n_dim) + ".txt")
     shift_vector = np.random.uniform(low, high)
     np.savetxt(data_path, shift_vector)
     return shift_vector
