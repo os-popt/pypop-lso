092ae4615b160fc5a8f73b4586a9b60e4d58ee90
os-popt <11749325@mail.sustech.edu.cn>
2020-04-24 22:38:16 +0800

    remove the function _transform_and_check_size and enhance the function _transform_and_check

--- benchmarks/continuous_functions.py
+++ benchmarks/continuous_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 # helper functions
-def _transform_and_check(x):
+def _transform_and_check(x, from_2_dim=False):
     """transform the input into the numpy.ndarray type, if necessary, and then
         check whether or not the number of dimensions <= 1. If not, raise a TypeError.
     """
@@ -10,59 +10,54 @@ def _transform_and_check(x):
     if x.ndim > 1:
         raise TypeError("the number of dimensions should <= 1 " +
             "after squeezing the input via numpy.squeeze().")
-    return x
-
-def _transform_and_check_size(x):
-    """Check whether or not the size >= 2. If not, raise a TypeError.
-    """
-    x = _transform_and_check(x)
-    if x.size < 2:
+    if from_2_dim and (x.size < 2):
         raise TypeError("the size should >= 2 after squeezing the input " +
             "via numpy.squeeze().")
     return x
 
 
+# unimodal functions
 def sphere(x):
     x = _transform_and_check(x)
     y = np.sum(np.power(x, 2))
     return y
 
 def cigar(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     x = np.power(x, 2)
     y = x[0] + (10 ** 6) * np.sum(x[1:])
     return y
 
 def discus(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     x = np.power(x, 2)
     y = (10 ** 6) * x[0] + np.sum(x[1:])
     return y
 
 def ellipsoid(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     weights = np.power(10, 6 * np.linspace(0, 1, num=x.size))
     y = np.dot(weights, np.power(x, 2))
     return y
 
 def parabolic_ridge(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     y = -x[0] + 100 * np.sum(np.power(x[1:], 2))
     return y
 
 def sharp_ridge(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     y = -x[0] + 100 * np.sqrt(np.sum(np.power(x[1:], 2)))
     return y
 
 def schwefel12(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     x = [np.sum(x[:i + 1]) for i in range(x.size)]
     y = np.sum(np.power(x, 2))
     return y
 
 def rosenbrock(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     y = 100 * np.sum(np.power(x[1:] - np.power(x[:-1], 2), 2)) + \
         np.sum(np.power(x[:-1] - 1, 2))
     return y
@@ -87,7 +82,7 @@ def rastrigin(x):
     return y
 
 def scaled_rastrigin(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     x = (10 ** np.linspace(0, 1, num=x.size)) * x
     y = rastrigin(x)
     return y
@@ -99,7 +94,7 @@ def skew_rastrigin(x):
     return y
 
 def schaffer(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     x = np.power(x, 2)
     x = x[:-1] + x[1:]
     y = np.sum(np.power(x, 0.25) * (np.power(np.sin(
@@ -112,7 +107,7 @@ def schwefel(x):
     return y
 
 def bohachevsky(x):
-    x = _transform_and_check_size(x)
+    x = _transform_and_check(x, True)
     xx = np.power(x, 2)
     y = np.sum(xx[:-1] + 2 * xx[1:] - 0.3 * np.cos(3 * np.pi * x[:-1]) -
         0.4 * np.cos(4 * np.pi * x[1:]) + 0.7)
