715035f219fe5e6a29876f0697a8db2c71ef69ce
os-popt <11749325@mail.sustech.edu.cn>
2020-04-28 20:52:58 +0800

    add the function _load_rotation_matrix

--- benchmarks/rotated_functions.py
+++ benchmarks/rotated_functions.py
@@ -5,6 +5,27 @@ from continuous_functions import _transform_and_check
 
 
 # helper functions
+def _load_rotation_matrix(func, x, rotation_matrix=None):
+    x = _transform_and_check(x)
+    if rotation_matrix is None:
+        if (not hasattr(func, "rotation_matrix")) or (func.rotation_matrix.shape != (x.size, x.size)):
+            data_path = os.path.join("po_input_data", "rotation_matrix___" +
+                func.__name__ + "_dim_" + str(x.size) + ".txt")
+            rotation_matrix = np.loadtxt(data_path)
+            func.rotation_matrix = rotation_matrix
+        rotation_matrix = func.rotation_matrix
+    else:
+        rotation_matrix = np.squeeze(rotation_matrix)
+    if rotation_matrix.shape != (x.size, x.size):
+        raise TypeError("rotation_matrix should have the same size as x " +
+            "after squeezing it via numpy.squeeze().")
+    if rotation_matrix.size != x.size * x.size:
+        raise TypeError("rotation_matrix should have the size: " +
+            str(x.size) + " * " + str(x.size) + " .")
+    x = np.dot(rotation_matrix, x)
+    return x
+
+
 def generate_rotation_matrix(func, n_dim):
     if hasattr(func, '__call__'):
         func = func.__name__
