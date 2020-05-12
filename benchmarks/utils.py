907bae397c56037d95f0c30b7e7e6f8c917b116f
os-popt <11749325@mail.sustech.edu.cn>
2020-05-12 22:32:40 +0800

    add the function plot_surface

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
@@ -24,5 +25,27 @@ def plot_contour(func, x, y, levels=None):
     plt.title(func.__name__)
     plt.xlabel("x")
     plt.ylabel("y")
+    plt.savefig(func.__name__ + ".png")
+    plt.show()
+
+
+def plot_surface(func, x, y):
+    x, y = np.array(x), np.array(y)
+    if (x.ndim != 1) or (y.ndim != 1):
+        raise TypeError("Both x and y should be a vector.")
+
+    [X, Y] = np.meshgrid(x, y)
+    Z = np.empty(X.shape)
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            Z[i, j] = func([X[i, j], Y[i, j]])
+
+    figure = plt.figure()
+    ax = Axes3D(figure)
+    ax.plot_surface(X, Y, Z, cmap=plt.cm.cool, alpha=0.9, edgecolors=None)
+
+    plt.title(func.__name__)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
     plt.savefig(func.__name__ + ".png")
     plt.show()
