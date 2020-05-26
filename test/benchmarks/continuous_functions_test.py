49abc57c233402191a1b4935590b99e898c21dfc
os-popt <11749325@mail.sustech.edu.cn>
2020-05-26 23:24:48 +0800

    test the function different_powers

--- test/benchmarks/continuous_functions_test.py
+++ test/benchmarks/continuous_functions_test.py
@@ -57,6 +57,17 @@ BenchmarkTest.check_via_sampling(ellipsoid, y, start_from=2)
 BenchmarkTest.check_origin(ellipsoid, start_from=2)
 print("")
 
+# different_powers
+y = [[68, 2, 0, 2, 68],
+    [84, 3, 0, 3, 84],
+    [0, 4, 4, 4, 4275.6, 4275.6, 81.3],
+    [0, 5, 5, 5, 16739, 16739, 203],
+    [0, 6, 6, 6, 51473.5, 51473.5, 437.1],
+    [0, 7, 7, 7, 133908.7, 133908.7, 847.4, 52736.8]]
+BenchmarkTest.check_via_sampling(different_powers, y, start_from=2)
+BenchmarkTest.check_origin(different_powers, start_from=2)
+print("")
+
 # schwefel12
 y = [[4, 1, 0, 5, 20],
     [8, 2, 0, 6, 24],
