a023b0532a725b7970952d72f4131cc517915bec
os-popt <11749325@mail.sustech.edu.cn>
2020-06-04 01:30:37 +0800

    update the class PureRandomSearch

--- optimizers/rs/prs.py
+++ optimizers/rs/prs.py
@@ -1,6 +1,7 @@
+import time
 import numpy as np
 
-from optimizer import PopulationOptimizer
+from optimizer import PopulationOptimizer, compress_fitness_data
 
 
 class PureRandomSearch(PopulationOptimizer):
@@ -15,32 +16,67 @@ class PureRandomSearch(PopulationOptimizer):
         PopulationOptimizer.__init__(self, problem, options)
     
     def optimize(self, fitness_function=None):
+        start_optimization = time.time()
+
         if (fitness_function is None) and (self.fitness_function != None):
                 fitness_function = self.fitness_function
         
         # initialize
         x = self._X # population with one individual
+        start_evaluation = time.time()
         y = fitness_function(x) # evaluate fitness of population
+        time_evaluations = time.time() - start_evaluation # time used for fitness evaluations
         n_evaluations = 1 # counter of fitness evaluations
         best_so_far_x = np.copy(x) # best-so-far solution
         best_so_far_y = np.copy(y) # best-so-far fitness
 
+        if self.save_fitness_data:
+            fitness_data = [y]
+        
         # iterate
+        termination = "max_evaluations" # by default
         while n_evaluations < self.max_evaluations:
             # sample uniformly and then evaluate
             x = self.rng.uniform(self.lower_boundary,
                 self.upper_boundary,
                 self._X_size)
+            start_evaluation = time.time()
             y = fitness_function(x)
+            time_evaluations += (time.time() - start_evaluation)
             n_evaluations += self.n_individuals
+            
+            if self.save_fitness_data:
+                fitness_data.append(y)
             
             # update best-so-far x and y
             if best_so_far_y > y:
                 best_so_far_x = np.copy(x)
                 best_so_far_y = np.copy(y)
+            
+            # check two termination criteria
+            runtime = time.time() - start_optimization
+            if runtime >= self.max_runtime:
+                termination = "max_runtime"
+                break
+            if best_so_far_y <= self.threshold_fitness:
+                termination = "threshold_fitness"
+                break
+        
+        if self.save_fitness_data:
+            start_compression = time.time()
+            fitness_data = compress_fitness_data(fitness_data, self.len_fitness_data)
+            time_compression = time.time() - start_compression
+        else:
+            fitness_data = None
+            time_compression = None
         
         results = {"best_so_far_x": best_so_far_x,
             "best_so_far_y": best_so_far_y,
-            "n_evaluations": n_evaluations}
+            "n_evaluations": n_evaluations,
+            "runtime": runtime,
+            "fitness_data": fitness_data,
+            "termination": termination,
+            "time_evaluations": time_evaluations,
+            "time_compression": time_compression}
         self._X = None # clear
         return results
