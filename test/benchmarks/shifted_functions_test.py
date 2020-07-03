1cee245a8254dc59de57dcfa74dd75cf2d5caf47
os-popt <11749325@mail.sustech.edu.cn>
2020-07-03 20:13:29 +0800

    test shifted discus

--- test/benchmarks/shifted_functions_test.py
+++ test/benchmarks/shifted_functions_test.py
@@ -23,3 +23,14 @@ y = [[4000004, 1000001, 0, 1000001, 4000004],
 BenchmarkTest.check_via_sampling(cigar, y, start_from=2, is_shifted=True)
 BenchmarkTest.check_origin(cigar, start_from=2, n_samples=100, is_shifted=True)
 print("")
+
+# shifted discus
+y = [[4000004, 1000001, 0, 1000001, 4000004],
+    [4000008, 1000002, 0, 1000002, 4000008],
+    [0, 1000003, 1000003, 1000003, 1000029, 1000029, 16000014],
+    [0, 1000004, 1000004, 1000004, 1000054, 1000054, 25000030],
+    [0, 1000005, 1000005, 1000005, 1000090, 1000090, 36000055],
+    [0, 1000006, 1000006, 1000006, 1000139, 1000139, 49000091, 91]]
+BenchmarkTest.check_via_sampling(discus, y, start_from=2, is_shifted=True)
+BenchmarkTest.check_origin(discus, start_from=2, n_samples=100, is_shifted=True)
+print("")
