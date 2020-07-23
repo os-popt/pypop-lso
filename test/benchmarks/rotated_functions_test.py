d64914aebcb32fe225a8a6a721d376daae530ab7
os-popt <11749325@mail.sustech.edu.cn>
2020-07-23 22:14:57 +0800

    test rotated cigar

--- test/benchmarks/rotated_functions_test.py
+++ test/benchmarks/rotated_functions_test.py
@@ -1,7 +1,12 @@
 from rotated_functions import *
 from test_cases import BenchmarkTest
 
-# sphere
-BenchmarkTest.check_origin(sphere, 
+# rotated sphere
+BenchmarkTest.check_origin(sphere,
+    end_with=1000, n_samples=10, is_rotated=True)
+print("")
+
+# rotated cigar
+BenchmarkTest.check_origin(cigar,
     end_with=1000, n_samples=10, is_rotated=True)
 print("")
