bbd0252101dd4eed522809f319e1393285094da9
os-popt <11749325@mail.sustech.edu.cn>
2020-08-06 20:21:15 +0800

    test rotated schaffer

--- test/benchmarks/rotated_functions_test.py
+++ test/benchmarks/rotated_functions_test.py
@@ -8,37 +8,37 @@ print("")
 
 # rotated cigar
 BenchmarkTest.check_origin(cigar,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated discus
 BenchmarkTest.check_origin(discus,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated cigar_discus
 BenchmarkTest.check_origin(cigar_discus,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated ellipsoid
 BenchmarkTest.check_origin(ellipsoid,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated different_powers
 BenchmarkTest.check_origin(different_powers,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated parabolic_ridge
 BenchmarkTest.check_origin(parabolic_ridge,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated sharp_ridge
 BenchmarkTest.check_origin(sharp_ridge,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated schwefel221
@@ -63,10 +63,15 @@ print("")
 
 # rotated scaled_rastrigin
 BenchmarkTest.check_origin(scaled_rastrigin,
-    end_with=1000, n_samples=10, is_rotated=True)
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
 
 # rotated skew_rastrigin
 BenchmarkTest.check_origin(skew_rastrigin,
     end_with=1000, n_samples=10, is_rotated=True)
+print("")
+
+# rotated schaffer
+BenchmarkTest.check_origin(schaffer,
+    start_from=2, end_with=1000, n_samples=10, is_rotated=True)
 print("")
