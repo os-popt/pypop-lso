6abe6f05711a71006c316d4b5d3f0f148ff124dc
os-popt <11749325@mail.sustech.edu.cn>
2020-08-08 23:33:55 +0800

    add the file optimizers/es/rechenberg.py

---
+++ optimizers/es/rechenberg.py
@@ -0,0 +1,82 @@
+import time
+import numpy as np
+
+from optimizer import compress_fitness_data
+from es import OnePlusOne
+
+
+class Rechenberg(OnePlusOne):
+    """Rechenberg's (1+1)-Evolution Strategy (Rechenberg's (1+1)-ES)."""
+    def __init__(self, problem, options):
+        options.setdefault("optimizer_name", "Rechenberg (Rechenberg's (1+1)-ES)")
+        n_individuals = options.get("n_individuals")
+        if (n_individuals != None) and (n_individuals != 1):
+            options["n_individuals"] = 1
+            print("For Rechenberg, only one individual is used to represent the population, " +\
+                "and the option 'n_individuals' has been reset to 1 (not {:d}).".format(n_individuals))
+        OnePlusOne.__init__(self, problem, options)
+    
+    def optimize(self, fitness_function=None):
+        start_optimization = time.time()
+
+        if (fitness_function is None) and (self.fitness_function != None):
+                fitness_function = self.fitness_function
+        
+        # initialize
+        x = np.copy(self._X) # population with one individual
+        self._X = None # to save memory space
+        start_evaluation = time.time()
+        y = fitness_function(x) # evaluate fitness of population
+        time_evaluations = time.time() - start_evaluation # time used for fitness evaluations
+        n_evaluations = 1 # counter of fitness evaluations
+        best_so_far_x = np.copy(x) # best-so-far solution
+        best_so_far_y = np.copy(y) # best-so-far fitness
+
+        if self.save_fitness_data:
+            fitness_data = [y]
+        
+        # iterate
+        termination = "max_evaluations" # by default
+        while n_evaluations < self.max_evaluations:
+            # sample from normal distribution and then evaluate
+            x = best_so_far_x + self.step_size *\
+                self.rng.standard_normal((self.ndim_problem,))
+            start_evaluation = time.time()
+            y = fitness_function(x)
+            time_evaluations += (time.time() - start_evaluation)
+            n_evaluations += 1
+            
+            if self.save_fitness_data:
+                fitness_data.append(y)
+            
+            # update best-so-far x and y
+            if best_so_far_y > y:
+                best_so_far_x = np.copy(x)
+                best_so_far_y = np.copy(y)
+            
+            # check two termination criteria
+            runtime = time.time() - start_optimization
+            if runtime >= self.max_runtime:
+                termination = "max_runtime"
+                break
+            if best_so_far_y <= self.threshold_fitness:
+                termination = "threshold_fitness"
+                break
+        
+        if self.save_fitness_data:
+            start_compression = time.time()
+            fitness_data = compress_fitness_data(fitness_data, self.len_fitness_data)
+            time_compression = time.time() - start_compression
+        else:
+            fitness_data = None
+            time_compression = None
+        
+        results = {"best_so_far_x": best_so_far_x,
+            "best_so_far_y": best_so_far_y,
+            "n_evaluations": n_evaluations,
+            "runtime": runtime,
+            "fitness_data": fitness_data,
+            "termination": termination,
+            "time_evaluations": time_evaluations,
+            "time_compression": time_compression}
+        return results
