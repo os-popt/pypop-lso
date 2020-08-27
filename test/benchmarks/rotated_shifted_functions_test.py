5ca7d74fc1ce0bae7d2f33f466da9600c84400cc
os-popt <11749325@mail.sustech.edu.cn>
2020-08-27 22:19:16 +0800

    test rotated shifted schwefel221

--- test/benchmarks/rotated_shifted_functions_test.py
+++ test/benchmarks/rotated_shifted_functions_test.py
@@ -40,3 +40,8 @@ print("")
 BenchmarkTest.check_origin(sharp_ridge,
     start_from=2, end_with=1000, n_samples=10, is_shifted=True, is_rotated=True)
 print("")
+
+# rotated shifted schwefel221
+BenchmarkTest.check_origin(schwefel221,
+    end_with=1000, n_samples=10, is_shifted=True, is_rotated=True)
+print("")
