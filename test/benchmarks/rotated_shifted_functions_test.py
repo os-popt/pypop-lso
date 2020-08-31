f8dc6621c4dc0fe76bcbbdad56fb9c730f42675f
os-popt <11749325@mail.sustech.edu.cn>
2020-08-31 23:44:05 +0800

    test rotated shifted scaled_rastrigin

--- test/benchmarks/rotated_shifted_functions_test.py
+++ test/benchmarks/rotated_shifted_functions_test.py
@@ -60,3 +60,8 @@ print("")
 BenchmarkTest.check_origin(rastrigin,
     end_with=1000, n_samples=10, is_shifted=True, is_rotated=True)
 print("")
+
+# rotated shifted scaled_rastrigin
+BenchmarkTest.check_origin(scaled_rastrigin,
+    start_from=2, end_with=1000, n_samples=10, is_shifted=True, is_rotated=True)
+print("")
