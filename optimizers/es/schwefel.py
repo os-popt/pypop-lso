014d1301fb73aea77f42580bf3d3714c84fcbb2e
os-popt <11749325@mail.sustech.edu.cn>
2020-09-10 21:32:26 +0800

    simplify the code of the file optimizers/es/schwefel.py

--- optimizers/es/schwefel.py
+++ optimizers/es/schwefel.py
@@ -17,11 +17,7 @@ class Schwefel(OnePlusOne):
     """
     def __init__(self, problem, options):
         options.setdefault("optimizer_name", "Schwefel (Schwefel's (1+1)-ES)")
-        n_individuals = options.get("n_individuals")
-        if (n_individuals != None) and (n_individuals != 1):
-            options["n_individuals"] = 1
-            print("For Schwefel, only one individual is used to represent the population, " + \
-                  "and the options 'n_individuals' has been reset to 1 (not {:d}).".format(n_individuals))
+        OnePlusOne._check_n_individuals(self, options, self.__class__.__name__)
         OnePlusOne.__init__(self, problem, options)
 
     def optimize(self, fitness_function=None):
