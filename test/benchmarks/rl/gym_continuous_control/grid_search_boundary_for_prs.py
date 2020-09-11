4d85bed2a42d33ab6f17a27dca8cf57c65b2c2ce
os-popt <11749325@mail.sustech.edu.cn>
2020-09-11 22:15:01 +0800

    update the script for better running

--- test/benchmarks/rl/gym_continuous_control/grid_search_boundary_for_prs.py
+++ test/benchmarks/rl/gym_continuous_control/grid_search_boundary_for_prs.py
@@ -3,6 +3,11 @@ from gym_continuous_control import grid_search_boundary
 from prs import PureRandomSearch
 
 start_time = time.time()
+grid_search_boundary(PureRandomSearch)
+print("$$$$$ total train time: {:.2e}.".format(
+    time.time() - start_time))
+
+start_time = time.time()
 grid_search_boundary(PureRandomSearch, is_test=True)
-print("$$$$$ total grid_search time: {:.2e}.".format(
+print("$$$$$ total test time: {:.2e}.".format(
     time.time() - start_time))
