399893fd2bf0fb4685119fcbd584571197baef69
os-popt <11749325@mail.sustech.edu.cn>
2020-09-27 22:47:50 +0800

    support restart

--- optimizers/es/ostermeier.py
+++ optimizers/es/ostermeier.py
@@ -6,7 +6,7 @@ from es import MuCommaLambda
 
 
 class Ostermeier(MuCommaLambda):
-    """Ostermeier's (1,lambda)-Evolution Strategy (Ostermeier's (1,lambda)-ES).
+    """Restart-based Ostermeier's (1,lambda)-Evolution Strategy (Ostermeier's (1,lambda)-ES).
     
     The concept of relatively large mutations within one generation,
     but passing only smaller variations to the next generation,
@@ -42,23 +42,48 @@ class Ostermeier(MuCommaLambda):
         n_evaluations = 1
         best_so_far_x, best_so_far_y = np.copy(parent), np.copy(y)
         history_x = np.hstack((n_evaluations, best_so_far_x))
-        Y = np.tile(y, (self.n_individuals,)) # fitness of population
-        
+
         if self.save_fitness_data:
             fitness_data = [y]
 
         # initialize variables involved for all individuals
-        X = np.empty((self.n_individuals, self.ndim_problem)) # population
-        delta = np.ones((self.ndim_problem,)) # individual step-sizes
-        xi = np.empty((self.n_individuals,)) # global step-size
-        Z = np.empty((self.n_individuals, self.ndim_problem)) # Gaussian noises
         beta = np.sqrt(1 / self.ndim_problem) # adaptation vs precision
         beta_scal = 1 / self.ndim_problem # in the range (0, 1)
         # beta_scal: small values facilitate a precise but time-consuming adaptaton
 
         # iterate
         termination = "max_evaluations"
+        is_restart, n_restart = True, 0
         while n_evaluations < self.max_evaluations:
+            if is_restart and (n_restart > 0):
+                parent = self.rng.uniform(
+                    self.lower_boundary, self.upper_boundary, (self.ndim_problem, ))
+                start_evaluation = time.time()
+                y = fitness_function(parent)
+                time_evaluations += (time.time() - start_evaluation)
+                n_evaluations += 1
+                if best_so_far_y > y:
+                    best_so_far_x = np.copy(parent)
+                    best_so_far_y = np.copy(y)
+                if self.save_best_so_far_x:
+                    if not (n_evaluations % self.freq_best_so_far_x):
+                        history_x = np.vstack((history_x,
+                            np.hstack((n_evaluations, best_so_far_x))))
+
+                if self.save_fitness_data:
+                    fitness_data.append(np.copy(y))
+
+                self.n_individuals *= 2
+                if self.n_individuals >= 1000:
+                    self.n_individuals = 1000
+                
+                Y = np.tile(y, (self.n_individuals,))  # fitness of population
+                X = np.empty((self.n_individuals, self.ndim_problem))  # population
+                delta = np.ones((self.ndim_problem,))  # individual step-sizes
+                xi = np.empty((self.n_individuals,))  # global step-size
+                Z = np.empty((self.n_individuals, self.ndim_problem))  # Gaussian noises
+                is_restart = False
+
             # 1. creation of lambda offspring
             for k in range(self.n_individuals):
                 if self.rng.uniform(0, 1, 1) > 0.5:
@@ -101,13 +126,19 @@ class Ostermeier(MuCommaLambda):
             delta *= (np.power(xi[sel], beta) *
                 np.power(np.exp(np.abs(Z[sel, :]) - np.sqrt(2 / np.pi)), beta_scal))
             
+            # check four termination criteria
             if np.min(delta) <= self.threshold_step_size:
-                termination = "threshold_step_size (lower)"
-                break
+                is_restart, n_restart = True, n_restart + 1
             if np.max(delta) >= 3 * np.min(self.upper_boundary - self.lower_boundary):
-                termination = "threshold_step_size (upper)"
+                is_restart, n_restart = True, n_restart + 1
+            runtime = time.time() - start_optimization
+            if runtime >= self.max_runtime:
+                termination = "max_runtime"
                 break
-            
+            if best_so_far_y <= self.threshold_fitness:
+                termination = "threshold_fitness"
+                break
+
         if self.save_fitness_data:
             start_compression = time.time()
             fitness_data = compress_fitness_data(fitness_data, self.len_fitness_data)
@@ -127,5 +158,6 @@ class Ostermeier(MuCommaLambda):
             "termination": termination,
             "time_evaluations": time_evaluations,
             "time_compression": time_compression,
-            "delta": delta}
+            "delta": delta,
+            "n_restart": n_restart}
         return results
