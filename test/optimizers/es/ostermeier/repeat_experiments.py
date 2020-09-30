bf75ff0242b1862f94eeafbf6351cbba42135e59
os-popt <11749325@mail.sustech.edu.cn>
2020-09-30 20:39:48 +0800

    add Figure 3 for Ostermeier

--- test/optimizers/es/ostermeier/repeat_experiments.py
+++ test/optimizers/es/ostermeier/repeat_experiments.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from continuous_functions import _transform_and_check
+from continuous_functions import _transform_and_check, schwefel12 as schwefel
 from ostermeier import Ostermeier
 
 
@@ -29,6 +29,7 @@ solver = Ostermeier(problem, options)
 results = solver.optimize(hyper_ellipsoid)
 print(results)
 fitness_data = results["fitness_data"]
+plt.figure(1)
 plt.plot(fitness_data[:, 0], fitness_data[:, 1])
 plt.title("Ostermeier's ES on Hyper-Ellipsoid")
 plt.xlabel("function evaluations")
@@ -39,3 +40,30 @@ plt.ylabel("best function value")
 plt.yscale("log")
 plt.yticks(np.logspace(-10, 4, 15))
 plt.savefig("Ostermeier-Figure-1.png")
+
+# Figure 3
+ndim_problem = 20
+problem = {"ndim_problem": ndim_problem,
+    "lower_boundary": -65 * np.ones((ndim_problem,)),
+    "upper_boundary": 65 * np.ones((ndim_problem,))}
+options = {"max_evaluations": 6e4,
+    "n_parents": n_parents,
+    "n_individuals": n_individuals,
+    "step_size": 0.3, # not given
+    "threshold_fitness": 1e-3,
+    "seed": 20200930} # not given
+solver = Ostermeier(problem, options)
+results = solver.optimize(schwefel)
+print(results)
+fitness_data = results["fitness_data"]
+plt.figure(3)
+plt.plot(fitness_data[:, 0], fitness_data[:, 1])
+plt.title("Ostermeier's ES on Schwefel's Problem")
+plt.xlabel("function evaluations")
+xticks = np.linspace(0, options["max_evaluations"], 7)
+xticks_label = ("{:.1E}".format(x) for x in xticks)
+plt.xticks(xticks, xticks_label)
+plt.ylabel("best function value")
+plt.yscale("log")
+plt.yticks(np.logspace(-3, 5, 9))
+plt.savefig("Ostermeier-Figure-3.png")
