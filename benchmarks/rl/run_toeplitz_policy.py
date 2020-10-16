08ce76f477e7c050cc0fb8bf1cd630845fb547bb
os-popt <11749325@mail.sustech.edu.cn>
2020-10-16 17:28:06 +0800

    fix a serious bug

--- benchmarks/rl/run_toeplitz_policy.py
+++ benchmarks/rl/run_toeplitz_policy.py
@@ -6,7 +6,7 @@ import gym
 import pickle
 
 
-# not show warning information
+# NOT show warning information for gym:
 #   https://github.com/openai/gym/blob/master/gym/logger.py
 gym.logger.setLevel(40)
 
@@ -32,9 +32,9 @@ class ToeplitzPolicy(object):
         self.h_dim = n_neurons
         self.ac_dim = n_outputs
         
-        self.w1 = np.zeros(self.ob_dim + self.h_dim - 1)
+        self.w1 = np.zeros(self.h_dim + self.ob_dim - 1)
         self.w2 = np.zeros(self.h_dim * 2 - 1)
-        self.w3 = np.zeros(self.ac_dim + self.h_dim - 1,)
+        self.w3 = np.zeros(self.ac_dim + self.h_dim - 1)
         self.b1 = np.zeros(self.h_dim) # bias
         self.b2 = np.zeros(self.h_dim)
         
@@ -69,10 +69,9 @@ class ToeplitzPolicy(object):
         self.W3 = self._build_layer(self.ac_dim, self.w3)
         
     def evaluate(self, X):
-        if len(X.shape) == 1:
-            X = X.reshape(X.shape[0], 1)
-        z1 = np.tanh(np.dot(self.W1, X) + self.b1)
-        z2 = np.tanh(np.dot(self.W2, z1) + self.b2)
+        if len(X.shape) == 1: X = X.reshape(X.shape[0], 1)
+        z1 = np.tanh(np.dot(self.W1, X) + self.b1.reshape(self.b1.size, 1))
+        z2 = np.tanh(np.dot(self.W2, z1) + self.b2.reshape(self.b2.size, 1))
         return np.squeeze(np.tanh(np.dot(self.W3, z2)))
 
 def _get_policy(n_inputs, n_neurons, n_outputs):
@@ -81,7 +80,7 @@ def _get_policy(n_inputs, n_neurons, n_outputs):
     return policy, ndim_problem
 
 def _get_actions(observation, policy):
-    actions = policy.evaluate(observation))
+    actions = policy.evaluate(observation)
     return actions
 
 def _cost_func(new_w, env, len_episode, policy):
@@ -128,13 +127,11 @@ class Experiment(object):
         self.datafile_suffix = params["datafile_suffix"]
 
         self.data_dir = "./env_{}_opt_{}".format(self.env_name, self.optimizer.__name__) # save results
-        if not os.path.exists(self.data_dir):
-            os.makedirs(self.data_dir)
+        if not os.path.exists(self.data_dir): os.makedirs(self.data_dir)
         self.datafile_prefix = "env_{}_opt_{}_maxeval_{}".format(
             self.env_name, self.optimizer.__name__, self.optimizer_options["max_evaluations"])
         
-        self.optimizer_options["txt_best_so_far_x"] =\
-            "{}/{}_history_best_so_far_x_{}.txt".format(
+        self.optimizer_options["txt_best_so_far_x"] ="{}/{}_history_best_so_far_x_{}.txt".format(
                 self.data_dir, self.datafile_prefix, self.datafile_suffix)
     
     def _save_train_results(self, results):
@@ -257,13 +254,12 @@ if __name__ == "__main__":
     if params["env_seed"] >= 0: # for simplicity
         rng = np.random.default_rng(params["env_seed"])
         # override three seeds even if they have been set
-        params["env_seed_train"], params["env_seed_test"],\
-            params["optimizer_options"]["seed"] = rng.integers(0, np.iinfo(np.int32).max, (3,))
+        params["env_seed_train"], params["env_seed_test"], params["optimizer_options"]["seed"] =\
+            rng.integers(0, np.iinfo(np.int32).max, (3,))
     
     # conduct experiment for one optimizer on one environment
     experiment = Experiment(params)
-    print("******* Environment [{}] + Optimizer [{}]:".format(
-        experiment.env_name, experiment.optimizer.__name__))
+    print("******* Environment [{}] + Optimizer [{}]:".format(experiment.env_name, experiment.optimizer.__name__))
     print("  * env_ndim_observation: {},".format(experiment.env_ndim_observation))
     print("  * env_ndim_actions: {},".format(experiment.env_ndim_actions))
     print("  * ndim_problem: {}, ".format(experiment.ndim_problem))
