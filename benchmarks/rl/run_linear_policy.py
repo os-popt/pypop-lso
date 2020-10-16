4a166c51cf3bac474c42db04d02da8b82f2bc104
os-popt <11749325@mail.sustech.edu.cn>
2020-10-16 17:03:27 +0800

    fix a serious bug

--- benchmarks/rl/run_linear_policy.py
+++ benchmarks/rl/run_linear_policy.py
@@ -6,17 +6,16 @@ import gym
 import pickle
 
 
-# not show warning information
+# NOT show warning information for gym and torch:
 #   https://github.com/openai/gym/blob/master/gym/logger.py
-gym.logger.setLevel(40)
+gym.logger.setLevel(40) # ERROR = 40
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 
 def _get_policy(n_inputs, n_outputs):
-    policy = torch.nn.Sequential(
-        torch.nn.Linear(n_inputs, n_outputs),
-        torch.nn.Tanh(),
-    )
+    policy = torch.nn.Sequential( # linear policy
+        torch.nn.Linear(n_inputs, n_outputs, bias=False),
+        torch.nn.Tanh())
     w = np.array([]) # flatten policy weights
     separators = dict()
     for k, v in policy.state_dict().items():
@@ -57,8 +56,7 @@ class Experiment(object):
         self.env_len_episode = params["env_len_episode"]
 
         self.env = gym.make(self.env_name)
-        self.env_ndim_observation =\
-            self.env.observation_space.shape[0]
+        self.env_ndim_observation = self.env.observation_space.shape[0]
         self.env_ndim_actions = self.env.action_space.shape[0]
 
         # set problem (policy) parameters
@@ -66,13 +64,10 @@ class Experiment(object):
         self.upper_boundary = params["problem_upper_boundary"]
 
         self.policy, self.separators, self.ndim_problem =\
-            _get_policy(self.env_ndim_observation,
-                self.env_ndim_actions)
+            _get_policy(self.env_ndim_observation, self.env_ndim_actions)
         self.problem = {"ndim_problem": self.ndim_problem,
-            "lower_boundary":\
-                self.lower_boundary * np.ones(self.ndim_problem),
-            "upper_boundary":\
-                self.upper_boundary * np.ones(self.ndim_problem)}
+            "lower_boundary": self.lower_boundary * np.ones(self.ndim_problem),
+            "upper_boundary": self.upper_boundary * np.ones(self.ndim_problem)}
         
         # set optimizer options
         self.optimizer_options = params["optimizer_options"]
@@ -82,41 +77,32 @@ class Experiment(object):
         self.n_test = params["n_test"]
         self.datafile_suffix = params["datafile_suffix"]
 
-        self.data_dir = "./env_{}_opt_{}".format( # save results
-            self.env_name, self.optimizer.__name__)
-        if not os.path.exists(self.data_dir):
-            os.makedirs(self.data_dir)
+        self.data_dir = "./env_{}_opt_{}".format(self.env_name, self.optimizer.__name__)
+        if not os.path.exists(self.data_dir): os.makedirs(self.data_dir)
         self.datafile_prefix = "env_{}_opt_{}_maxeval_{}".format(
-            self.env_name, self.optimizer.__name__,
-            self.optimizer_options["max_evaluations"])
+            self.env_name, self.optimizer.__name__, self.optimizer_options["max_evaluations"])
         
         self.optimizer_options["txt_best_so_far_x"] =\
             "{}/{}_history_best_so_far_x_{}.txt".format(
-                self.data_dir, self.datafile_prefix,
-                self.datafile_suffix)
+                self.data_dir, self.datafile_prefix, self.datafile_suffix)
     
     def _save_train_results(self, results):
         np.savetxt("{}/{}_best_so_far_x_{}.txt".format(
-            self.data_dir, self.datafile_prefix,
-            self.datafile_suffix), results["best_so_far_x"])
+            self.data_dir, self.datafile_prefix, self.datafile_suffix), results["best_so_far_x"])
         np.savetxt("{}/{}_fitness_data_{}.txt".format(
-            self.data_dir, self.datafile_prefix,
-            self.datafile_suffix), results["fitness_data"])
+            self.data_dir, self.datafile_prefix, self.datafile_suffix), results["fitness_data"])
         results_file = "{}/{}_results_{}.pickle".format(
             self.data_dir, self.datafile_prefix, self.datafile_suffix)
         with open(results_file, "wb") as results_handle:
-            pickle.dump(results, results_handle,
-                protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(results, results_handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     def _load_best_so_far_x(self):
         return np.loadtxt("{}/{}_best_so_far_x_{}.txt".format(
-            self.data_dir, self.datafile_prefix,
-            self.datafile_suffix))
+            self.data_dir, self.datafile_prefix, self.datafile_suffix))
     
     def _save_test_results(self, rewards):
         np.savetxt("{}/{}_rewards_{}.txt".format(
-            self.data_dir, self.datafile_prefix,
-            self.datafile_suffix), rewards)
+            self.data_dir, self.datafile_prefix, self.datafile_suffix), rewards)
 
     def train(self, is_test=False):
         # set the cost function (to be minimized)
@@ -135,8 +121,7 @@ class Experiment(object):
         
         if not is_test:
             # optimize the cost function
-            solver = self.optimizer(
-                self.problem, self.optimizer_options)
+            solver = self.optimizer(self.problem, self.optimizer_options)
             results = solver.optimize(_cost)
             self._save_train_results(results)
         else:
@@ -157,38 +142,26 @@ if __name__ == "__main__":
 
     # set environment parameters
     parser.add_argument('--env_name', '-en', type=str)
-    #  when --env_seed is set, --env_seed_train and --env_seed_test
-    #    will be automatically set (for simplicity)
-    parser.add_argument('--env_seed', '-es',
-        type=int, default=-1) # should set it as a nonnegative int
-    parser.add_argument('--env_seed_train', '-estr',
-        type=int, default=-1) # should set it as a nonnegative int
-    parser.add_argument('--env_seed_test', '-este',
-        type=int, default=-1) # should set it as a nonnegative int
-    parser.add_argument('--env_len_episode', '-ele',
-        type=int, default=1000)
+    #  when --env_seed is set, --env_seed_train and --env_seed_test will be automatically set (for simplicity)
+    parser.add_argument('--env_seed', '-es', type=int, default=-1) # should set it as a nonnegative int
+    parser.add_argument('--env_seed_train', '-estr', type=int, default=-1) # should set it as a nonnegative int
+    parser.add_argument('--env_seed_test', '-este', type=int, default=-1) # should set it as a nonnegative int
+    parser.add_argument('--env_len_episode', '-ele', type=int, default=1000)
 
     # set problem parameters
-    parser.add_argument('--problem_lower_boundary', '-plb',
-        type=float, default=-0.1)
-    parser.add_argument('--problem_upper_boundary', '-pub',
-        type=float, default=0.1)
+    parser.add_argument('--problem_lower_boundary', '-plb', type=float, default=-0.1)
+    parser.add_argument('--problem_upper_boundary', '-pub', type=float, default=0.1)
     
     # set optimizer options
     parser.add_argument('--optimizer_name', '-on', type=str)
 
-    parser.add_argument('--max_evaluations', '-me',
-        type=int, default=50000)
-    #  when --env_seed is set, --optimizer_seed
-    #    will be automatically set (for simplicity)
-    parser.add_argument('--optimizer_seed', '-os',
-        type=int, default=-1) # should set it as a nonnegative int
+    parser.add_argument('--max_evaluations', '-me', type=int, default=50000)
+    #  when --env_seed is set, --optimizer_seed will be automatically set (for simplicity)
+    parser.add_argument('--optimizer_seed', '-os', type=int, default=-1) # should set it as a nonnegative int
     
     # set experiment parameters
-    parser.add_argument('--n_test', '-nt',
-        type=int, default=100)
-    parser.add_argument('--datafile_suffix', '-ds',
-        type=str, default="_1")
+    parser.add_argument('--n_test', '-nt', type=int, default=100)
+    parser.add_argument('--datafile_suffix', '-ds', type=str, default="")
     
     # parse all parameters
     args = parser.parse_args()
@@ -223,46 +196,35 @@ if __name__ == "__main__":
         raise ValueError('cannot find {}.'.format(optimizer_name))
     params["optimizer"] = Optimizer
     
-    optimizer_options = {
-        "max_evaluations": params.pop("max_evaluations"),
+    optimizer_options = {"max_evaluations": params.pop("max_evaluations"),
         "seed": params.pop("optimizer_seed"),
         "save_best_so_far_x": True,
-        "step_size": 0.3 * (params["problem_upper_boundary"] -
-            params["problem_lower_boundary"])}
+        "step_size": 0.3 * (params["problem_upper_boundary"] - params["problem_lower_boundary"])}
     params["optimizer_options"] = optimizer_options
     
     if params["env_seed"] >= 0: # for simplicity
         rng = np.random.default_rng(params["env_seed"])
         # override three seeds even if they have been set
-        params["env_seed_train"], params["env_seed_test"],\
-            params["optimizer_options"]["seed"] = rng.integers(
-                0, np.iinfo(np.int32).max, (3,))
+        params["env_seed_train"], params["env_seed_test"], params["optimizer_options"]["seed"] =\
+            rng.integers(0, np.iinfo(np.int32).max, (3,))
     
     # conduct experiment for one optimizer on one environment
     experiment = Experiment(params)
-    print("******* Environment [{}] + Optimizer [{}]:".format(
-        experiment.env_name, experiment.optimizer.__name__))
-    print("  * env_ndim_observation: {},".format(
-        experiment.env_ndim_observation))
-    print("  * env_ndim_actions: {},".format(
-        experiment.env_ndim_actions))
+    print("******* Environment [{}] + Optimizer [{}]:".format(experiment.env_name, experiment.optimizer.__name__))
+    print("  * env_ndim_observation: {},".format(experiment.env_ndim_observation))
+    print("  * env_ndim_actions: {},".format(experiment.env_ndim_actions))
     print("  * ndim_problem: {}, ".format(experiment.ndim_problem))
     print("  * env_seed_train: {},".format(experiment.env_seed_train))
     print("  * env_seed_test: {},".format(experiment.env_seed_test))
-    print("  * max_evaluations: {},".format(
-        experiment.optimizer_options["max_evaluations"]))
-    print("  * optimizer_seed: {},".format(
-        experiment.optimizer_options["seed"]))
+    print("  * max_evaluations: {},".format(experiment.optimizer_options["max_evaluations"]))
+    print("  * optimizer_seed: {},".format(experiment.optimizer_options["seed"]))
 
     train_start_time = time.time()
     experiment.train()
-    print(" $$$$$ train time: {:.2e},".format(
-        time.time() - train_start_time))
+    print(" $$$$$ train time: {:.2e},".format(time.time() - train_start_time))
     
     test_start_time = time.time()
     experiment.test()
-    print(" $$$$$ test time: {:.2e},".format(
-        time.time() - test_start_time))
+    print(" $$$$$ test time: {:.2e},".format(time.time() - test_start_time))
     
-    print("$$$$$$$ Total Runtime: {:.2e}.".format(
-        time.time() - start_time))
+    print("$$$$$$$ Total Runtime: {:.2e}.".format(time.time() - start_time))
