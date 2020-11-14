cc6f133f38c6271accb43b65614033bf10e77ef6
os-popt <11749325@mail.sustech.edu.cn>
2020-11-14 23:56:39 +0800

    simplify optimizers/es/ostermeier.py

--- optimizers/es/ostermeier.py
+++ optimizers/es/ostermeier.py
@@ -1,7 +1,6 @@
 import time
 import numpy as np
 
-from optimizer import compress_fitness_data
 from es import MuCommaLambda
 
 
@@ -32,20 +31,15 @@ class Ostermeier(MuCommaLambda):
             fitness_function = self.fitness_function
 
         # initialize
-        if self._X.ndim == 1:
-            parent = np.copy(self._X)
-        else:
-            parent = np.copy(self._X[0, :]) # discard all other individuals
-        self._X = None # clear
+        parent = MuCommaLambda._get_m(self)
         start_evaluation = time.time()
         y = fitness_function(parent)
-        time_evaluations = time.time() - start_evaluation
-        n_evaluations = 1
+        n_evaluations, time_evaluations = 1, time.time() - start_evaluation
         best_so_far_x, best_so_far_y = np.copy(parent), np.copy(y)
-        history_x = np.hstack((n_evaluations, best_so_far_x))
 
-        if self.save_fitness_data:
-            fitness_data = [y]
+        if self.save_fitness_data: fitness_data = [y]
+        if self.save_best_so_far_x: history_x = np.hstack((n_evaluations, best_so_far_x))
+        else: history_x = None
 
         # initialize variables involved for all individuals
         beta = np.sqrt(1 / self.ndim_problem) # adaptation vs precision
@@ -64,20 +58,13 @@ class Ostermeier(MuCommaLambda):
                     y = fitness_function(parent)
                     time_evaluations += (time.time() - start_evaluation)
                     n_evaluations += 1
-                    if best_so_far_y > y:
-                        best_so_far_x = np.copy(parent)
-                        best_so_far_y = np.copy(y)
-                    if self.save_best_so_far_x:
-                        if not (n_evaluations % self.freq_best_so_far_x):
-                            history_x = np.vstack((history_x,
-                                np.hstack((n_evaluations, best_so_far_x))))
-                    
-                    if self.save_fitness_data:
-                        fitness_data.append(np.copy(y))
+                    if best_so_far_y > y: best_so_far_x, best_so_far_y = np.copy(parent), np.copy(y)
+                    if self.save_best_so_far_x and not(n_evaluations % self.freq_best_so_far_x):
+                        history_x = np.vstack((history_x, np.hstack((n_evaluations, best_so_far_x))))
+                    if self.save_fitness_data: fitness_data.append(np.copy(y))
                     
                     self.n_individuals *= 2
-                    if self.n_individuals >= 1000:
-                        self.n_individuals = 1000
+                    if self.n_individuals >= 1000: self.n_individuals = 1000
                 
                 Y = np.tile(y, (self.n_individuals,))  # fitness of population
                 X = np.empty((self.n_individuals, self.ndim_problem))  # population
@@ -88,10 +75,8 @@ class Ostermeier(MuCommaLambda):
 
             # 1. creation of lambda offspring
             for k in range(self.n_individuals):
-                if self.rng.uniform(0, 1, 1) > 0.5:
-                    xi[k] = 1.4
-                else:
-                    xi[k] = 1 / 1.4
+                if self.rng.uniform(0, 1, 1) > 0.5: xi[k] = 1.4
+                else: xi[k] = 1 / 1.4
                 Z[k, :] = self.rng.standard_normal((self.ndim_problem,))
                 X[k, :] = parent + xi[k] * delta * Z[k, :]
 
@@ -100,27 +85,17 @@ class Ostermeier(MuCommaLambda):
                 time_evaluations += time.time() - start_evaluation
                 n_evaluations += 1
 
-                if self.save_fitness_data:
-                    fitness_data.append(Y[k])
+                if self.save_fitness_data: fitness_data.append(Y[k])
                 
                 # update best-so-far x and y
-                if best_so_far_y > Y[k]:
-                    best_so_far_x, best_so_far_y = np.copy(X[k, :]), np.copy(Y[k])
-                if self.save_best_so_far_x:
-                    if not(n_evaluations % self.freq_best_so_far_x):
-                        history_x = np.vstack((history_x,
-                            np.hstack((n_evaluations, best_so_far_x))))
+                if best_so_far_y > Y[k]: best_so_far_x, best_so_far_y = np.copy(X[k, :]), np.copy(Y[k])
+                if self.save_best_so_far_x and not(n_evaluations % self.freq_best_so_far_x):
+                    history_x = np.vstack((history_x, np.hstack((n_evaluations, best_so_far_x))))
 
                 # check three termination criteria
-                if n_evaluations >= self.max_evaluations:
-                    break
-                runtime = time.time() - start_optimization
-                if runtime >= self.max_runtime:
-                    termination = "max_runtime"
-                    break
-                if best_so_far_y <= self.threshold_fitness:
-                    termination = "threshold_fitness"
-                    break
+                is_break, termination = MuCommaLambda._check_terminations(
+                    self, n_evaluations, time.time() - start_optimization, best_so_far_y)
+                if is_break: break
             
             # 2. selection / adaptation
             sel = np.argmin(Y)
@@ -128,30 +103,18 @@ class Ostermeier(MuCommaLambda):
             delta *= (np.power(xi[sel], beta) *
                 np.power(np.exp(np.abs(Z[sel, :]) - np.sqrt(2 / np.pi)), beta_scal))
             
-            # check four termination criteria
+            # check termination criteria
+            runtime = time.time() - start_optimization
+            is_break, termination = MuCommaLambda._check_terminations(
+                    self, n_evaluations, runtime, best_so_far_y)
+            if is_break: break
             if np.min(delta) <= self.threshold_step_size:
                 is_restart, n_restart = True, n_restart + 1
             if np.max(delta) >= 3 * np.min(self.upper_boundary - self.lower_boundary):
                 is_restart, n_restart = True, n_restart + 1
-            runtime = time.time() - start_optimization
-            if runtime >= self.max_runtime:
-                termination = "max_runtime"
-                break
-            if best_so_far_y <= self.threshold_fitness:
-                termination = "threshold_fitness"
-                break
 
-        if self.save_fitness_data:
-            start_compression = time.time()
-            fitness_data = compress_fitness_data(fitness_data, self.len_fitness_data)
-            time_compression = time.time() - start_compression
-        else:
-            fitness_data = None
-            time_compression = None
-        
-        if self.save_best_so_far_x:
-            np.savetxt(self.txt_best_so_far_x, history_x)
-        
+        fitness_data, time_compression = MuCommaLambda._save_data(self, history_x, fitness_data)
+
         results = {"best_so_far_x": best_so_far_x,
             "best_so_far_y": best_so_far_y,
             "n_evaluations": n_evaluations,
