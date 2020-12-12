a2419753449cee9610dfb6f814f00c3d0ffe4560
os-popt <11749325@mail.sustech.edu.cn>
2020-12-12 20:08:28 +0800

    standardize optimizers/es/rrm.py

--- optimizers/es/rrm.py
+++ optimizers/es/rrm.py
@@ -5,7 +5,7 @@ from es import MuCommaLambda
 
 
 class RestartRm(MuCommaLambda):
-    """Restart-based Rm Evolution Strategy (R-Rm-ES) for large-scale, black-box optimization.
+    """Restart-based Rank-m Evolution Strategy (R-Rm-ES) for large-scale, black-box optimization.
     
     Reference
     ---------
@@ -17,14 +17,23 @@ class RestartRm(MuCommaLambda):
     def __init__(self, problem, options):
         options.setdefault("optimizer_name", "RestartRm (R-Rm-ES)")
         MuCommaLambda.__init__(self, problem, options)
-        self.threshold_step_size = 1e-10
-        self.c_cov = 1 / (3 * np.sqrt(problem["ndim_problem"]) + 5)
-        self.c_c = 2 / (problem["ndim_problem"] + 7)
-        self.q_star = 0.3
-        self.c_s = 0.3
-        self.d_sigma = 1
-        self.n_evolution_paths = 2 # m
-        self.T = problem["ndim_problem"]
+        # learning (changing) rate of covariance matrix
+        self.c_cov = options.get("c_cov", 1 / (3 * np.sqrt(problem["ndim_problem"]) + 5))
+        # learning (changing) rate of principal search direction (evolution path)
+        self.c = options.get("c", 2 / (problem["ndim_problem"] + 7))
+        # learning (changing) rate of cumulative rank rate
+        self.c_s = options.get("c_s", 0.3)
+        # target ratio for mutation strength adaptation
+        self.q_star = options.get("q_star", 0.3)
+        # damping factor for mutation strength adaptation
+        self.d_sigma = options.get("d_sigma", 1)
+        # number of multiple evolution paths
+        self.n_evolution_paths = options.get("n_evolution_paths", 2) # m
+        # generation gap for multiple evolution paths
+        self.T = options.get("T", problem["ndim_problem"])
+
+        # threshold of step-size for restart
+        self.threshold_step_size = options.get("threshold_step_size", 1e-10)
     
     def optimize(self, fitness_function=None):
         start_optimization = time.time()
@@ -32,67 +41,69 @@ class RestartRm(MuCommaLambda):
         if (fitness_function is None) and (self.fitness_function != None):
             fitness_function = self.fitness_function
         
-        # initialize
-        m = MuCommaLambda._get_m(self)
+        # initialize distribution mean
+        # Here not confuse distribution mean (m) with number of multiple evolution paths (m)
+        m = MuCommaLambda._get_m(self) # distribution mean
         start_evaluation = time.time()
         y = fitness_function(m)
-        n_evaluations, time_evaluations = 1, time.time() - start_evaluation
+        time_evaluations, n_evaluations = time.time() - start_evaluation, 1
         best_so_far_x, best_so_far_y = np.copy(m), np.copy(y)
-
         if self.save_fitness_data: fitness_data = [y]
         if self.save_best_so_far_x: history_x = np.hstack((n_evaluations, best_so_far_x))
         else: history_x = None
 
-        # iterate
-        n_evolution_paths = self.n_evolution_paths
+        # iterate / evolve
         termination = "max_evaluations"
-        a = np.sqrt(1 - self.c_cov)
+        n_evolution_paths = self.n_evolution_paths # m
+        # Here introduce 4 new constant symbols to simplify code
+        # constant symbols for Line 4 of Algorithm 3
+        a, b = np.sqrt(1 - self.c_cov), np.sqrt(self.c_cov)
         a_m = a ** n_evolution_paths
-        b = np.sqrt(self.c_cov)
-        p_c1 = 1 - self.c_c
+        # constant symbols for Line 11 of Algorithm 3
+        p_1 = 1 - self.c
         is_restart, n_restart = True, 0
         while n_evaluations < self.max_evaluations:
             if is_restart:
+                is_restart = False
                 if n_restart > 0:
-                    m = self.rng.uniform(self.lower_boundary,
-                        self.upper_boundary, (self.ndim_problem,))
+                    m = self.rng.uniform(self.lower_boundary, self.upper_boundary, (self.ndim_problem,))
                     start_evaluation = time.time()
                     y = fitness_function(m)
                     time_evaluations += (time.time() - start_evaluation)
                     n_evaluations += 1
-                    if self.save_fitness_data: fitness_data.append(np.copy(y))
+
                     if best_so_far_y > y: best_so_far_x, best_so_far_y = np.copy(m), np.copy(y)
+                    if self.save_fitness_data: fitness_data.append(np.copy(y))
                     if self.save_best_so_far_x and not(n_evaluations % self.freq_best_so_far_x):
                         history_x = np.vstack((history_x, np.hstack((n_evaluations, best_so_far_x))))
                     
-                    self.n_individuals = self.n_individuals * 2
+                    self.n_individuals *= 2
                     self.n_parents = int(np.floor(self.n_individuals / 2))
-                    self.d_sigma = self.d_sigma * 2
+                    self.d_sigma *= 2
                 
-                # set weights for parents
-                w = np.log(np.arange(1, self.n_parents + 1))
-                w = (np.log(self.n_parents + 1) - w) / (
-                    self.n_parents * np.log(self.n_parents + 1) - np.sum(w))
+                # set weights for parents selection
+                w, w_1 = np.log(np.arange(1, self.n_parents + 1)), np.log(self.n_parents + 1)
+                w = (w_1 - w) / (self.n_parents * w_1 - np.sum(w))
+                # normalization factor for principal search direction adaptation
                 mu_eff = 1 / np.sum(np.power(w, 2))
-                W = np.tile(w[:, np.newaxis], (1, self.ndim_problem))
                 
+                sigma = self.step_size # mutation strength (global step-size)
                 # multiple evolution paths
-                MEP = np.zeros((n_evolution_paths, self.ndim_problem))
-                t_hat = np.zeros((n_evolution_paths,))
-                p = np.zeros((self.ndim_problem,))
+                MEP = np.zeros((n_evolution_paths, self.ndim_problem)) # P
+                t_hat = np.zeros((n_evolution_paths,)) # direction set for Algorithm 2
+                p = np.zeros((self.ndim_problem,)) # principal search direction (evolution path)
+                s = 0 # cumulative rank rate
+                t = 0 # generation index
+                p_2 = np.sqrt(self.c * (2 - self.c) * mu_eff)
+                RR = np.arange(1, self.n_parents * 2 + 1) # ranks for R_t, R_(t+1)
+
                 X = np.empty((self.n_individuals, self.ndim_problem)) # population
-                Y = np.tile(y, (self.n_individuals,))
-                RR = np.arange(1, self.n_parents * 2 + 1) # ranks
-                p_c2 = np.sqrt(self.c_c * (2 - self.c_c) * mu_eff)
-                s = 0
-                sigma = self.step_size
-                t = 0
-                
-                is_restart = False
+                Y = np.tile(y, (self.n_individuals,)) # fitness of population
             
             Y_bak = np.copy(Y)
-            for i in range(self.n_individuals):
+            for i in range(self.n_individuals): # one generation
                 z = self.rng.standard_normal((self.ndim_problem,))
+                # sample (Line 4 of Algorithm 3)
                 sum_p = np.zeros((self.ndim_problem,))
                 for j in range(1, n_evolution_paths + 1):
                     r = self.rng.standard_normal()
@@ -104,36 +115,38 @@ class RestartRm(MuCommaLambda):
                 n_evaluations += 1
                 Y[i] = y
                 
-                if self.save_fitness_data: fitness_data.append(np.copy(y))
-                
                 # update best-so-far x and y
                 if best_so_far_y > y: best_so_far_x, best_so_far_y = np.copy(X[i, :]), np.copy(y)
+                if self.save_fitness_data: fitness_data.append(np.copy(y))
                 if self.save_best_so_far_x and not(n_evaluations % self.freq_best_so_far_x):
                     history_x = np.vstack((history_x, np.hstack((n_evaluations, best_so_far_x))))
                 
                 # check three termination criteria
+                runtime = time.time() - start_optimization
                 is_break, termination = MuCommaLambda._check_terminations(
-                    self, n_evaluations, time.time() - start_optimization, best_so_far_y)
+                    self, n_evaluations, runtime, best_so_far_y)
                 if is_break: break
             
-            # check three termination criteria
-            runtime = time.time() - start_optimization
+            # check four termination criteria
             is_break, termination = MuCommaLambda._check_terminations(
-                    self, n_evaluations, runtime, best_so_far_y)
-            if is_break: break
-            if sigma <= self.threshold_step_size:
+                self, n_evaluations, runtime, best_so_far_y, sigma)
+            if termination == "threshold_step_size (lower)":
                 is_restart, n_restart = True, n_restart + 1
+            elif is_break: break
             
             # update distribution mean
             index = np.argsort(Y)
-            X, Y = X[index, :], Y[index]
+            X, Y = X[index, :], Y[index] # Line 9 of Algorithm 3
             m_bak = m
-            m = np.sum(X[:self.n_parents, :] * W, 0)
-
-            # update principal evolution path
-            p = p_c1 * p + p_c2 * ((m - m_bak) / sigma)
+            # Line 10 of Algorithm 3
+            m = np.zeros((self.ndim_problem,))
+            for j in range(self.n_parents):
+                m += w[j] * X[j, :]
             
-            # update multiple evolution paths
+            # update principal search direction (Line 11 of Algorithm 3)
+            p = p_1 * p + p_2 * ((m - m_bak) / sigma)
+            
+            # update multiple evolution paths (Algorithm 2 or Line 12 of Algorithm 3)
             T_min = np.min(np.diff(t_hat))
             if (T_min > self.T) or (t < n_evolution_paths):
                 for i in range(n_evolution_paths - 1):
@@ -144,15 +157,15 @@ class RestartRm(MuCommaLambda):
                     MEP[i, :], t_hat[i] = MEP[i + 1, :], t_hat[i + 1]
             MEP[n_evolution_paths - 1, :] = p
             t_hat[n_evolution_paths - 1] = t
-            t += 1
+            t += 1 # Line 14 of Algorithm 3
 
-            # update step-size
+            # adapt mutation strength (rank-based success rule, RSR) -> Line 13 of Algorithm 3
             F = np.hstack((Y_bak[:self.n_parents], Y[:self.n_parents]))
             R = np.argsort(F)
-            q = np.sum(w * (RR[R < self.n_parents] -
-                RR[R >= self.n_parents])) / self.n_parents
+            R_t, R_t1 = RR[R < self.n_parents], RR[R >= self.n_parents]
+            q = np.sum(w * (R_t - R_t1)) / self.n_parents
             s = (1 - self.c_s) * s + self.c_s * (q - self.q_star)
-            sigma = sigma * np.exp(s / self.d_sigma)
+            sigma *= np.exp(s / self.d_sigma)
         
         fitness_data, time_compression = MuCommaLambda._save_data(self, history_x, fitness_data)
 
@@ -166,7 +179,9 @@ class RestartRm(MuCommaLambda):
             "time_compression": time_compression,
             "m": m,
             "p": p,
+            "s": s,
             "step_size": sigma,
+            "t_hat": t_hat,
             "n_individuals": self.n_individuals,
             "n_parents": self.n_parents,
             "d_sigma": self.d_sigma,
